e6164c5460d8f9be86479c50316e15bc22ae5dd8
DaElias <58747265+DaElias@users.noreply.github.com>
2021-12-30 17:15:22 -0500

    add .

--- tareas-node/app.js
+++ tareas-node/app.js
@@ -1,16 +1,16 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+// const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const {inquireMenu, pausa} = require('./helpers/inquire');
 console.clear();
 
 let opt = '';
 
 const main = async () => {
-
     do {
-        opt = await mostrarMenu();
-
-        if (opt !== '0') await pausa();
-    } while (opt !== '0');
+        opt = await inquireMenu();
+        console.log(opt)
+        await pausa();
+    } while (opt.option != '0');
 
 }
 
---
+++ tareas-node/helpers/inquire.js
@@ -0,0 +1,74 @@
+const inquire = require('inquirer');
+require('colors');
+
+const opt = [
+    {
+        value: '1',
+        name: '1. '.red + 'Crear una tarea.'
+    },
+    {
+        value: '2',
+        name: '2. '.red + 'Listar tarea.'
+    },
+    {
+        value: '3',
+        name: '3. '.red + 'Lista tarea completada.'
+    },
+    {
+        value: '4',
+        name: '4. '.red + 'Lista tarea pendiente.'
+    },
+    {
+        value: '5',
+        name: '5. '.red + 'Completar tareas.'
+    },
+    {
+        value: '6',
+        name: '6. '.red + 'Borrar tareas.'
+    },
+    {
+        value: 0,
+        name: '0. '.red + 'Salir\n'.rainbow
+    }
+];
+const preguntas = [
+    {
+        type: 'list',
+        name: 'option',
+        message: 'Que desea hacer?',
+        choices: opt
+    }
+];
+
+const inquireMenu = async () => {
+    console.clear();
+    console.log('========================='.rainbow);
+    console.log(' Selecione una opcion '.blue);
+    console.log('=========================\n'.rainbow);
+    return await inquire.prompt(preguntas);
+}
+
+const pausa = async () => {
+    console.log('\n'); 
+    return await inquire.prompt(
+        [
+            {
+                type: 'list',
+                name: 'option',
+                message: `Precione ${'Enter'.rainbow} para continuar ${'...'.green}`,
+                choices:[{
+                    type: 'input',
+                    name: 'Enter'.rainbow,
+                    message: '...'.rainbow,
+                    choices: [{}]
+                }]
+
+            }
+        ]
+    )
+}
+
+module.exports = {
+    inquireMenu,
+    pausa
+};
